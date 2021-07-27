de541ea9332dd95e603753cb004a153873b2cb7a
Tommi Nieminen <software@legisign.org>
2021-07-27 13:58:54 +0300

    More f-strings, enums in parser. (0.9.6)

--- srthandler/__init__.py
+++ srthandler/__init__.py
@@ -4,13 +4,15 @@
   srthandler -- classes and methods for SubRip (.srt) handling
 
   2021-07-18  0.9.5  Utilizing f-strings.
+  2021-07-27  0.9.6  More f-strings, enums.
 
 '''
 
 import re
 import codecs
+import enum
 
-version = '0.9.5'
+version = '0.9.6'
 
 # HELPER FUNCTIONS
 
@@ -23,6 +25,8 @@ def to_secs(time):
     '''
     # .srt files use a decimal comma (actually, a decimal period works
     # too but let’s standardize here!)
+    if isinstance(time, (float, int)):
+        return time
     if time.startswith('-'):
         sign = -1
         time = time[1:]
@@ -69,19 +73,28 @@ class TimeLineError(ParseError):
 
 # CLASSES
 
+class ParserState(enum.Enum):
+    '''State of the parser.'''
+    REC = enum.auto()       # waiting for the record number
+    TIME = enum.auto()      # waiting for the timepoint
+    TEXT = enum.auto()      # waiting for the text
+
 class SubtextEntry(object):
     '''Object holding a single subtitle text.
 
     Object’s properties are the in-time and out-time the subtitle (in
     seconds), and the subtitle text as a list of strings.'''
 
-    def __init__(self, intime=0.0, outtime=0.0, text=None):
-        self.intime = intime
-        self.outtime = outtime
+    def __init__(self, intime=0, outtime=0, text=None):
+        self.intime = to_secs(intime)
+        self.outtime = to_secs(outtime)
         self.text = text
 
-    def __str__(self):
-        return f'{to_timestr(self.intime)} --> {to_timestr(self.outtime)}\n{self.text}'
+    def __repr__(self):
+        intime = to_timestr(self.intime)
+        outtime = to_timestr(self.outtime)
+        text = self.text if self.text else ''
+        return f'{intime} --> {outtime}\n{text}'
 
     @property
     def dur(self):
@@ -94,27 +107,19 @@ class SubtextEntry(object):
 
     @intime.setter
     def intime(self, intime):
-        if isinstance(intime, (int, float)):
-            self.__intime = intime
-        elif isinstance(intime, str):
-            self.__intime = to_secs(intime)
-        else:
-            raise TypeError(intime)
+        self.__intime = to_secs(intime)
 
     def move_by(self, offset):
         '''Move subtitle by offset (seconds).'''
-        if isinstance(offset, str):
-            offset = to_secs(offset)
-        self.intime += offset
-        self.outtime += offset
+        self.intime += to_secs(offset)
+        self.outtime += to_secs(offset)
 
     def move_to(self, pos=0.0):
         '''Move subtitle to pos (seconds).'''
-        if isinstance(pos, str):
-            pos = to_secs(pos)
-        dur = self.dur
+        pos = to_secs(pos)
+        curr_dur = self.dur
         self.intime = pos
-        self.outtime = pos + dur
+        self.outtime = pos + curr_dur
 
     @property
     def outtime(self):
@@ -122,12 +127,7 @@ class SubtextEntry(object):
 
     @outtime.setter
     def outtime(self, outtime):
-        if isinstance(outtime, (int, float)):
-            self.__outtime = outtime
-        elif isinstance(outtime, str):
-            self.__outtime = to_secs(outtime)
-        else:
-            raise TypeError(outtime)
+        self.__outtime = to_secs(outtime)
 
     @property
     def text(self):
@@ -152,36 +152,33 @@ class SubtextLayer(list):
         if filename:
             self.read(filename)
 
-    def __str__(self):
-        return '\n'.join(['{}\n{}\n'.format(i + self.start_from, str(s)) \
+    def __repr__(self):
+        return '\n'.join([f'{i + self.start_from}\n{s}\n' \
             for i, s in enumerate(self)])
 
     def check(self):
         '''A simple check of the correctness of the subtext layer.'''
         log = {}
-        for no, pair in enumerate(zip(self, self[1:])):
+        for lineno, pair in enumerate(zip(self, self[1:]), 2):
             prev, this = pair
             if this.intime <= prev.intime:
-                log[no + 2] = 'before or at previous entry'
+                log[lineno] = 'before or at previous entry'
             if this.dur <= 0:
-                log[no + 2] = 'duration <= 0'
+                log[lineno] = 'duration <= 0'
         return log
 
     def insert(self, intime=0.0, outtime=0.0, dur=0.0, text=None):
         '''Insert a new record in the layer.'''
-        if isinstance(intime, str):
-            intime = to_secs(intime)
-        if isinstance(outtime, str):
-            outtime = to_secs(outtime)
-        if isinstance(dur, str):
-            dur = to_secs(dur)
+        intime = to_secs(intime)
+        outtime = to_secs(outtime)
+        dur = to_secs(dur)
         if outtime < 0.001 and dur > 0.001:
             outtime = intime + dur
-        new = SubtextEntry(intime, outtime, text)
+        new = SubtextEntry(intime=intime, outtime=outtime, text=text)
         if self == []:
             self.append(new)
         elif self[-1].intime < intime:
-            # Fix overlapping instantly
+            # Fix overlap instantly
             if self[-1].outtime > intime:
                 self[-1].outtime = intime
             self.append(new)
@@ -190,7 +187,7 @@ class SubtextLayer(list):
                 if ref.intime > intime:
                     break
             super().insert(i, new)
-            # Fix overlaps instantly
+            # Fix overlap instantly
             if ref.outtime > intime:
                 ref.outtime = intime
 
@@ -220,48 +217,42 @@ class SubtextLayer(list):
         '''Parse buffer as SRT data.'''
         timeline = re.compile('^(?P<intime>\d+:\d+:\d+[,.]\d+)\s+-->\s+'
                               '(?P<outtime>\d+:\d+:\d+[,.]\d+)$')
-        # State can be one of the following:
-        #   rec# : waiting for record #
-        #   time : waiting for timepoint
-        #   text : waiting for actual content
-        state = 'rec#'
+        state = ParserState.REC
         curr = None
         text = []
-        for lineno, line in enumerate(buff):
+        for lineno, line in enumerate(buff, 1):
             line = line.strip()
-            if state == 'rec#':
+            if state == ParserState.REC:
                 if not line.isnumeric():
                     raise IndexLineError(lineno)
                 curr = SubtextEntry()
-                state = 'time'
-            elif state == 'time':
+                state = ParserState.TIME
+            elif state == ParserState.TIME:
                 m = timeline.match(line)
                 if not m:
                     raise TimeLineError(lineno)
                 curr.intime = m.group('intime')
                 curr.outtime = m.group('outtime')
-                state = 'text'
-            elif state == 'text':
-                if not line:
-                    if text:
-                        curr.text = text
-                        text = []
-                    self.append(curr)
-                    state = 'rec#'
-                    curr = None
-                else:
+                state = ParserState.TEXT
+            elif state == ParserState.TEXT:
+                if line:
                     text.append(line)
+                else:
+                    curr.text = text
+                    self.append(curr)
+                    state = ParserState.REC
+                    curr = None
+                    text = []
         # Handle the last entry
         if curr:
-            if text:
-                curr.text = text
+            curr.text = text
             self.append(curr)
 
     def read(self, filename):
         '''Read given file and forward it to parse().'''
         self.filename = filename
         try:
-            # UTF-8 with (or without) BOM
+            # UTF-8 with or without BOM
             with codecs.open(filename, 'r', 'utf_8_sig') as f:
                 self.parse(f)
         except UnicodeDecodeError:
@@ -275,13 +266,15 @@ class SubtextLayer(list):
             startpoint = to_secs(startpoint)
         if isinstance(endpoint, str):
             endpoint = to_secs(endpoint)
+        assert isinstance(startpoint, (float, int))
+        assert isinstance(endpoint, (float, int))
         first, *_, last = self
         first.intime = startpoint
         first.outtime = startpoint + first.dur
         for sub in self[1:]:
-            dur = sub.dur
+            curr_dur = sub.dur
             sub.intime = (endpoint * sub.intime) / last.intime
-            sub.outtime = sub.intime + dur
+            sub.outtime = sub.intime + curr_dur
 
     def write(self, filename=None):
         '''Write the layer to a file.'''
